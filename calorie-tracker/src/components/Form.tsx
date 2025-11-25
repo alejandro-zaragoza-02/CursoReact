607d76cd0b9a8a083c26dea08c487a48677603c0
Alejandro Zaragoza <102815776+alejandro-zaragoza-02@users.noreply.github.com>
2025-11-25 16:31:26 +0100

    Fin del proyecto de contador de calorias

---
+++ calorie-tracker/src/components/Form.tsx
@@ -0,0 +1,112 @@
+import {
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type Dispatch,
+  type FormEvent,
+} from "react";
+import { categories } from "../data/categories";
+import type { Activity } from "../types";
+import type {
+  ActivityActions,
+  ActivityState,
+} from "../reducers/activity-reducer";
+import { v4 as uuid } from "uuid";
+
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
+};
+
+export default function Form({ dispatch, state }: FormProps) {
+  const initialState: Activity = {
+    id: uuid(),
+    category: 1,
+    name: "",
+    calories: 0,
+  };
+
+  const [activity, setActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      setActivity(selectedActivity);
+    }
+  }, [state.activeId]);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const target = e.target as HTMLSelectElement | HTMLInputElement;
+    const isNumberField = ["category", "calories"].includes(target.id);
+    setActivity({
+      ...activity,
+      [target.id]: isNumberField ? +target.value : target.value,
+    });
+  };
+
+  const isValidActivity = () => {
+    const { name, calories } = activity;
+    return name.trim() !== "" && calories > 0;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "save-activity", payload: { newActivity: activity } });
+    setActivity(initialState);
+  };
+
+  return (
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoría:</label>
+        <select
+          id="category"
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name">Actividad:</label>
+        <input
+          id="name"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Zumo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta..."
+          value={activity.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories">Calorías:</label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorías. ej. 300, 500..."
+          value={activity.calories}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value={activity.category === 1 ? "Guardar comida" : "Guardar ejercicio"}
+        disabled={!isValidActivity()}
+      />
+    </form>
+  );
+}
---
+++ calorie-tracker/src/reducers/activity-reducer.ts
@@ -0,0 +1,73 @@
+import type { Activity } from "../types";
+
+export type ActivityActions =
+  | {
+      type: "save-activity";
+      payload: { newActivity: Activity };
+    }
+  | {
+      type: "set-activeId";
+      payload: { id: Activity["id"] };
+    }
+  | {
+      type: "detele-activity";
+      payload: { id: Activity["id"] };
+    }
+  | { type: "restart-app" };
+
+export type ActivityState = {
+  activities: Activity[];
+  activeId: Activity["id"];
+};
+
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem("activities");
+  return activities ? JSON.parse(activities) : [];
+};
+
+export const initialState: ActivityState = {
+  activities: localStorageActivities(),
+  activeId: "",
+};
+
+export const activityReducer = (
+  state: ActivityState = initialState,
+  action: ActivityActions
+): ActivityState => {
+  if (action.type === "save-activity") {
+    let updatedActivities: Activity[] = [];
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity];
+    }
+    return {
+      ...state,
+      activities: updatedActivities,
+      activeId: "",
+    };
+  } else if (action.type === "set-activeId") {
+    console.log(action.payload.id);
+
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  } else if (action.type === "detele-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    };
+  } else if (action.type === "restart-app") {
+    return {
+      activities: [],
+      activeId: "",
+    };
+  }
+
+  return state;
+};
